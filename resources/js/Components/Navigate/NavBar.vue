51a18b347c463ae32a30954ea0653cb1f98c0bd4
Tiago Alves <tiagooliveiraaso2@gmail.com>
2024-01-06 13:53:54 -0300

    :sparkles: new:  layout inicial navbar

---
+++ resources/js/Components/Navigate/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="navbar">
+        <div class="menu-hamburger">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+        <div class="options">
+            <i class="fa-solid fa-bell"></i>
+            <i class="fa-solid fa-gear"></i>
+            <img src="../../../../public/img/profile-default.png" />
+        </div>
+        <!-- <div class="profile">
+                <img src="../../../../public/img/profile-default.png" style="width:50px" />
+            </div> -->
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss"></style>
